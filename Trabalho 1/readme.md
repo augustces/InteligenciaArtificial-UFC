254aa3b25a43c530873b35376b3517de8bb59488
augustces <augustces@alu.ufc.br>
2024-01-09 19:31:51 -0300

    Trabalho 1 - Concluído

---
+++ Trabalho 1/readme.md
@@ -0,0 +1,22 @@
+# Trabalho 1 - Subida de Encosta
+
+Problema: Caixeiro Viajante
+
+## Desenvolvedores
+
+- [Augusto César](https://github.com/augustces)
+
+## Arquivos Importantes
+
+O enunciado do problema está no arquivo [Enunciado - Subida de Encosta.pdf]().
+
+Os testes foram executados no arquivo [Testes_01.ipynb](). O arquivo contém a aplicação do algoritmo para um vetor de coordenadas
+x e y. O algoritmo foi executado 30 vezes em cada variação (são 8) e foi mostrado os resultados de cada execução utilizando um dataframe, utilizando a biblioteca "pandas".
+
+O algoritmo está no arquivo [trabalho.py](). É necessário ler de um arquivo de entrada as coordenadas x e y de cada cidade, para calcular os pesos correspondentes, que serão as distâncias entre uma cidade e outra. A partir dos inputs solicitados ao rodar a aplicação, a variação do algoritmo correspondente executa o algoritmo Subida de Encosta e retorna o valor de melhor custo e a melhor rota, representada pela ordem de cidades no arquivo de leitura.
+
+O arquivo de entrada, contendo as cidades que serão consideradas para o cálculo estão no arquivo [entrada.txt]().
+
+As instruções de execução estão no arquivo [help.txt]().
+
+O relatório final está no arquivo [Relatório - Trabalho 1 - IA.pdf]().
--- Trabalho 1/trabalho.py
+++ Trabalho 1/trabalho.py
@@ -1,147 +1,156 @@
 import random
 import math
 
+
 # Função para gerar um vizinho trocando dois pontos na rota
-def operador1(rota):
-    vizinho = rota[:]
-    i, j = random.sample(range(len(rota)), 2)
-    vizinho[i], vizinho[j] = vizinho[j], vizinho[i]
+def operador1(rota, i, j):
+    vizinho = rota[:]  # cópia da rota
+    vizinho[i], vizinho[j] = (
+        vizinho[j],
+        vizinho[i],
+    )  # troca a cidade que estava no índice i para o índice j, e vice-versa
     return vizinho
 
+
 # Função para inverter trechos das permutações entre dois pontos da rota
-def operador2(rota):
-    vizinho = rota[:]
-    i, j = random.sample(range(len(rota)), 2)
-    if i > j:
-        i, j = j, i
-    vizinho[i:j+1] = reversed(vizinho[i:j+1])
+def operador2(rota, i, j):
+    tam = len(rota)  # tamanho da rota
+    vizinho = rota[:]  # cópia da rota
+    if i > j:  # caso i seja maior que j (propriedade circular da lista)
+        for c in vizinho[
+            : j - i
+        ]:  # aumenta a lista para auxiliar na inversão de trechos
+            vizinho.append(c)
+        while j >= 0:  # realiza a troca de cidades (garante a inversão)
+            vizinho[j], vizinho[i] = vizinho[i], vizinho[j]
+            i += 1
+            j -= 1
+        vizinho = vizinho[:tam]  # transforma de volta a lista para seu tamanho original
+    else:
+        while i < j:  # caso i menor que j (trecho interno da rota)
+            vizinho[i], vizinho[j] = (
+                vizinho[j],
+                vizinho[i],
+            )  # faz troca de cidades (garante a inversão)
+            i += 1
+            j -= 1
+            if i == tam:  # garante índices dentro do intervalo
+                i == 0
+            if j < 0:  # garante índices dentro do intervalo
+                j = tam - 1
     return vizinho
+
 
 # Função para calcular a distância entre duas cidades
 def distancia(cidade1, cidade2):
-    return math.sqrt((cidade1[0] - cidade2[0])**2 + (cidade1[1] - cidade2[1])**2)
+    return math.sqrt((cidade1[0] - cidade2[0]) ** 2 + (cidade1[1] - cidade2[1]) ** 2)
+
 
 # Função para calcular o custo total de uma rota
 def calcular_custo(rota, pesos):
     custo = 0.0
-    for i in range(len(rota) - 1):
+    # percorre toda a rota somando os custos de uma cidade a outra
+    # ainda calcula o custo da última cidade para a cidade de origem
+    for i in range(len(rota)):
         cidade_atual = rota[i]
-        proxima_cidade = rota[i + 1]
+        proxima_cidade = rota[
+            (i + 1) % len(rota)
+        ]  # garante que o índice não saia do invervalo
         custo += pesos[cidade_atual][proxima_cidade]
     return custo
 
-# Função que calcula o fatorial
-def fatorial(n):
-    if n == 1 :
-        return 1
-    return n*(n-1)
 
 # Função para gerar vizinhos com base em uma rota
-def calcular_vizinhos (cidades, rota, f):
-    vizinhos = []
-    possivel = rota[:]
-    possivel = f(possivel)
-    for _ in range(fatorial(len(cidades))):
-        possivel = f(possivel)
-        if ((possivel not in vizinhos) or len(vizinhos) == 0): 
-            vizinhos.append(possivel)
+def calcular_vizinhos(cidades, rota, operador):
+    vizinhos = []  # lista de vizinhos
+    for i in range(
+        len(cidades)
+    ):  # para cada cidade, calcula novos vizinhos com base na rota e no operador escolhido
+        for j in range(len(cidades)):
+            vizinhos.append(operador(rota, i, j))
     return vizinhos
 
+
 # Subida de Encosta
-def subida_de_encosta(cidades, iteracoes,f, pesos,  inicialRandom = True, randomizar = True):
-    coluna = []
-    inicial = list(range(len(cidades))) # estado inicial é a sequencia lida no arquivo
-    if (inicialRandom): # caso o estado inicial seja gerado aleatoriamente, embaralhamos a sequencia lida no arquivo
+def subida_de_encosta(cidades, operador, pesos, inicialRandom=True, randomizar=True):
+    inicial = list(range(len(cidades)))  # estado inicial é a sequencia lida no arquivo
+    if (
+        inicialRandom
+    ):  # caso o estado inicial seja gerado aleatoriamente, embaralhamos a sequencia lida no arquivo
         random.shuffle(inicial)
-    melhor_rota = inicial
-    melhor_custo = calcular_custo(inicial, pesos) 
-    coluna.append((melhor_custo, melhor_rota))
-    for _ in range(iteracoes-1):
-        vizinhos = calcular_vizinhos(cidades, melhor_rota, f) 
-        if (randomizar):
+    melhor_rota = inicial[:]  # melhor rota atual é a inicial
+    melhor_custo = calcular_custo(inicial, pesos)  # melhor custo atual
+
+    condicao = True  # condição de parada do loop
+    # executa até não haver um vizinho com custo melhor
+    while condicao:
+        condicao = False  # Até agora nenhum vizinho foi melhor
+        vizinhos = calcular_vizinhos(
+            cidades, melhor_rota, operador
+        )  # encontra os vizinhos possíveis
+
+        if randomizar:  # se a variação for para randomizar os vizinhos, randomiza
             random.shuffle(vizinhos)
-        for vizinho in vizinhos :
-            custo_vizinho = calcular_custo(vizinho, pesos)
-            if custo_vizinho < melhor_custo:
-                melhor_rota = vizinho
-                melhor_custo = custo_vizinho
-                break 
-        melhores = ((melhor_custo, melhor_rota)) 
-        coluna.append(melhores)
+        # ver o primeiro vizinho com custo menor
+        for vizinho in vizinhos:
+            custo_vizinho = calcular_custo(vizinho, pesos)  # calcula o custo do vizinho
+            if (
+                custo_vizinho < melhor_custo
+            ):  # verifica se o custo do vizinho é menor que o melhor custo
+                melhor_rota = vizinho  # atualiza o valor da melhor rota
+                melhor_custo = custo_vizinho  # atualiza o valor do melhor custo
+                condicao = True  # deixa True para verificar os próximos vizinhos
+                break
 
-    return coluna
+    return melhor_custo, melhor_rota
 
-# Função para escrever a matriz de resultados de cada interação de cada varização
-def escrever(matriz, interacoes, variacoes):
-    fout = open("saida.txt", "w")
-    fout.write("Interacao \t \t 1 \t \t 2 \t\t 3 \t\t 4\t \t 5\t \t 6 \t\t 7 \t\t 8 \n")
-    for interacao in range(interacoes):
-        msg = f"{interacao + 1} \t\t"
-        fout.write(msg)
-        for variacao in range(variacoes):
-            msg = f"{ matriz[variacao][interacao] } \t\t" 
-            fout.write(msg)
-        fout.write("\n")
-    fout.close()
 
 # Função para ler as coordenadas das cidades
 def leitura():
-    fin = open("entrada.txt", "r")
-    x = list(map(float, fin.readline().split())) 
-    y = list(map(float, fin.readline().split()))
-    fin.close()
-    cidades = [ (x[i], y[i])  for i in range (len(x))]
+    fin = open("entrada.txt", "r")  # lê as cidades do arquivo de entrada
+    x = list(map(float, fin.readline().split()))  # guarda os valores de x
+    y = list(map(float, fin.readline().split()))  # guarda os valores de y
+    fin.close()  # fecha o arquivo
+    cidades = [(x[i], y[i]) for i in range(len(x))]  # organiza as cidades
     pesos = []
+    # calcula os pesos (a distância) de uma cidade para outra
     for cidade1 in cidades:
         coluna = []
         for cidade2 in cidades:
-            coluna.append(distancia(cidade1, cidade2)) 
+            coluna.append(distancia(cidade1, cidade2))
         pesos.append(coluna)
+    # retorna as coordenadas das cidades e os pesos de cada cidade
     return cidades, pesos
 
 
-def main ():
+def main():
     # Cidades lidas do arquivo de entrada
     cidades, pesos = leitura()
-    # Número de iterações
-    iteracoes = 30
-    #Matriz que será gerada no arquivo de saída
-    matriz = []
 
-    # Estado Inicial 1 com Operador 1 sem randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador1,pesos, False, False)
-    matriz.append(coluna)
+    print(
+        "Cidades lidas, informe qual operador usar \n 1 (troca duas cidades de posição em um circuito) \n 2 (inverte o trajeto entre duas cidades em um circuito) \n"
+    )
+    operador = int(input())
 
-    # Estado Inicial 1 com Operador 1 com randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador1,pesos, False)
-    matriz.append(coluna)
+    print("Os vizinhos serão comparados randomicamente? Sim = 1, Não = 0")
+    yes = int(input())
 
-    # Estado Inicial 1 com Operador 2 sem randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador2,pesos, False, False)
-    matriz.append(coluna)
+    print("A cidade inicial é arbitrária? Sim = 1, Não = 0")
+    inicialRandom = int(input())
 
-    # Estado Inicial 1 com Operador 2 com randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador2,pesos, False)
-    matriz.append(coluna)
+    if operador == 1:
+        melhor_custo, melhor_rota = subida_de_encosta(
+            cidades, operador1, pesos, inicialRandom == 1, yes == 1
+        )
+        print(f"Melhor custo: {melhor_custo}")
+        print(f"Melhor rota: {melhor_rota}")
+    else:
+        melhor_custo, melhor_rota = subida_de_encosta(
+            cidades, operador2, pesos, inicialRandom == 1, yes == 1
+        )
+        print(f"Melhor custo: {melhor_custo}")
+        print(f"Melhor rota: {melhor_rota}")
 
-    # Estado Inicial 2 com Operador 1 sem randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador1,pesos, True, False)
-    matriz.append(coluna)
-
-    # Estado Inicial 2 com Operador 1 com randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador1,pesos)
-    matriz.append(coluna)
-
-    # Estado Inicial 2 com Operador 2 sem randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador2,pesos, True, False)
-    matriz.append(coluna)
-
-    # Estado Inicial 2 com Operador 2 com randomização da vizinhança
-    coluna = subida_de_encosta(cidades, iteracoes, operador2,pesos)
-    matriz.append(coluna)
-
-    # Chamada de função para escrever a matriz no arquivo de saída 
-    escrever(matriz, 30, 8)
 
 if __name__ == "__main__":
     main()
